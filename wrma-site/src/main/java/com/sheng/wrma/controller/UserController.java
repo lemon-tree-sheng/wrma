cf9aa9eb515e127e9e01c483c471c12359ecdf81
shengxingyue <shengxingyue@souche.com>
2017-11-16 10:05:12 +0800

    接口返回 json 数据

--- wrma-site/src/main/java/com/sheng/wrma/controller/UserController.java
+++ wrma-site/src/main/java/com/sheng/wrma/controller/UserController.java
@@ -2,24 +2,23 @@ package com.sheng.wrma.controller;
 
 import com.sheng.wrma.modal.UserDO;
 import com.sheng.wrma.model.constant.Result;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author shengxingyue, created on 2017/11/12
  */
-@Controller
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
-    @RequestMapping(value = "/user.do", method = RequestMethod.GET)
-    @ResponseBody
-    public Result getUser() {
-        System.out.println("id:");
+    @GetMapping("/{id}")
+    public Result getUser(@PathVariable Integer id) {
         UserDO userDO = new UserDO();
-        userDO.setId(1);
-        userDO.setName("sheng");
+        userDO.setId(id);
+        userDO.setName("sheng:" + id);
         return Result.success(userDO);
     }
 }
